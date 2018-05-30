9269169d5f170b47899d9d9fe1ea0646870459b6
Your Name <mederbek.abdyldaev@iaau.edu.kg>
2018-05-30 08:59:47 +0600

    First Commit

---
+++ src/main/java/com/ourFinalProject/turizm/JailooApplication.java
@@ -0,0 +1,12 @@
+package com.ourFinalProject.turizm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JailooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JailooApplication.class, args);
+	}
+}
